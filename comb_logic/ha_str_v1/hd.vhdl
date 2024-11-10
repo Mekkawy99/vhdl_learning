c0e1ddd85ec34dd3aded1ec4004f7a44cd89272d
Mekkawy99 <melmekawy@zewailcity.edu.eg>
2024-11-10 14:27:45 +0100

    Updated version of HA ver1. Compilation using ghdl failed! as std_logic of xor2 is not declared! It is declared but maybe the compiler has an issue in compiling such files

--- comb_logic/ha_str_v1/hd.vhdl
+++ comb_logic/ha_str_v1/hd.vhdl
@@ -1,70 +1,55 @@
--- Design: Half adder using strcutural method
--- Concepts: Structual methods of writing codes
+-- Design: Half adder using structural method
+-- Concepts: Structural methods of writing VHDL code
 -- Date: 10.11.2024
 -- Engineer: Mohamed Elmekawy
 
 library ieee;
 use ieee.std_logic_1164.all;
 
-
+-- and2 Entity and Architecture
 entity and2 is
-port ( a,b: in std_logic;
-c: out std_logic
-);
+    port ( a, b : in std_logic;
+           c : out std_logic );
 end and2;
 
 architecture and_arch of and2 is
 begin
-process (a,b) begin
-c <= a and b;
-end process;
+    -- AND gate operation
+    c <= a and b;
 end and_arch;
 
-
+-- xor2 Entity and Architecture
 entity xor2 is
-port ( a,b: in std_logic;
-c: out std_logic
-);
+    port ( a, b : in std_logic;
+           c : out std_logic );
 end xor2;
 
 architecture xor_arch of xor2 is
--- components
 begin
-process (a,b) begin
-c <= a xor b;
-end process;
-
+    -- XOR gate operation
+    c <= a xor b;
 end xor_arch;
 
-
+-- half_add Entity and Architecture
 entity half_add is
-port ( a,b: in std_logic;
-sum,carry: out std_logic
-);
+    port ( a, b : in std_logic;
+           sum, carry : out std_logic );
 end half_add;
 
 architecture hd_arch of half_add is
--- component
-
-component and2
-        port (
-            a, b : in std_logic;
-            c    : out std_logic
-        );
+    -- Component Declarations
+    component and2
+        port ( a, b : in std_logic;
+               c : out std_logic );
     end component;
 
     component xor2
-        port (
-            a, b : in std_logic;
-            c    : out std_logic
-        );
+        port ( a, b : in std_logic;
+               c : out std_logic );
     end component;
 
-    -- Signals for connecting components
-    signal xor_out : std_logic;
 begin
     -- Component Instantiations
     xor_inst: xor2 port map(a => a, b => b, c => sum);
     and_inst: and2 port map(a => a, b => b, c => carry);
-
 end hd_arch;
